009507b304d4cb3c7b718d32ba6ea9858b2c2e2c
Caduyz <ccadu895@gmail.com>
2025-03-15 21:43:33 -0300

    Version 3

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
-let s0, v0, a, halfTime, t = 0;
-let animating = false;
-let pistaMetros = 1000;
-let lastGraphUpdate = 0;
-let stopped = true;
-let carro = 'carro1';
+let s0, v0, a, track, carro, midTrack;
+let t = 0;
+let animando = false;
+let framesCount = 0;
 
-const carros = {
+const carros = { // objeto com os carros e seus parametros
     carro1: {
-        link: 'https://static.vecteezy.com/system/resources/thumbnails/023/524/637/small_2x/red-sport-car-design-transparent-background-free-png.png',
-        width: 94,
-        height:32,
-        direction: 'right',
-        posY: 211,
+        img: 'https://images.vexels.com/media/users/3/258907/isolated/preview/63b3084a2e3abf46c1ff1f1163278631-transporte-de-carro-esporte-amarelo.png',
+        width: 120,
+        height: 220,
+        direction: 'left',
+        posY: (window.innerWidth * 0.21) -142,
     },
     carro2: {
-        link: 'https://static.vecteezy.com/system/resources/thumbnails/001/193/929/small_2x/vintage-car.png',
-        width: 94,
-        height:34,
+        img: 'https://images.vexels.com/media/users/3/243727/isolated/preview/961170707cab73907669cc9af41209fb-transporte-notebookdoodle-colorvinyl-cr-3.png',
+        width: 120,
+        height: 220,
         direction: 'right',
-        posY: 211,
+        posY: (window.innerWidth * 0.21) -146,
     },
     carro3: {
-        link: 'https://images.vexels.com/media/users/3/258907/isolated/preview/63b3084a2e3abf46c1ff1f1163278631-transporte-de-carro-esporte-amarelo.png',
+        img: 'https://static.vecteezy.com/system/resources/thumbnails/023/524/637/small_2x/red-sport-car-design-transparent-background-free-png.png',
         width: 110,
-        height:132,
-        direction: 'left',
-        posY: 163,
-    },
-    carro4: {
-        link: 'https://images.vexels.com/media/users/3/243727/isolated/preview/961170707cab73907669cc9af41209fb-transporte-notebookdoodle-colorvinyl-cr-3.png',
-        width: 110,
-        height:122,
+        height: 56,
         direction: 'right',
-        posY: 165,
+        posY: (window.innerWidth * 0.21) -61,
     }
 }
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth * 0.651;
-canvas.height = 300;
-const dt = 1/60;
+const canvas = document.getElementById("canvas"); // cria o canvas (tela) onde o carro será animado
+canvas.width = window.innerWidth * 0.88;
+canvas.height = window.innerWidth * 0.21;
+const ctx = canvas.getContext("2d"); // define o canvas como 2d
+let dt = Number((1 / 60).toFixed(10));
 
-const carroImg = new Image();
+const carroImg = new Image(); // cria a imagem do carro
+
+function selecionarCarro(idCarro) { // funcao para selecionar o carro
+    carroImg.src = carros[idCarro].img; // define a imagem do carro com base no objeto com os carros
+    carroImg.onload = function() { // garante que a imagem do carro vai ter carregado antes de aplicar as dimensoes
+        carroImg.width = carros[idCarro].width;
+        carroImg.height = carros[idCarro].height;
+    };
+    document.querySelectorAll('.car').forEach(element => element.classList.remove('active')); // retira a borda ao redor dos carros nao selecionados
+    document.getElementById(idCarro).classList.add('active'); // coloca a borda ao redor do carro selecionado
+    carro = idCarro;
+    moverCarro();
+}
+
+window.onload = function() {
+    selecionarCarro('carro1');
+};
 
 // gráfico de velocidade
 const ctxSpeedChart = document.getElementById("speedChart").getContext("2d");
@@ -56,8 +63,8 @@ const speedChart = new Chart(ctxSpeedChart, {
             borderColor: 'rgba(75, 192, 192, 1)',
             borderWidth: 2,
             fill: false,
-            tension: 0.1,
-            pointRadius: 0 // Remove os pontos nos gráficos
+            tension: 0,
+            pointRadius: 0
         }]
     },
     options: {
@@ -65,51 +72,19 @@ const speedChart = new Chart(ctxSpeedChart, {
         maintainAspectRatio: false,
         plugins: {
             legend: {
-                display: true, // Mantém a legenda visível
+                display: true, 
                 labels: {
-                    usePointStyle: false, // Remove o botão de esconder dataset
-                    boxWidth: 0 // Remove a caixinha ao lado do nome
+                    usePointStyle: false,
+                    boxWidth: 0
                 }
             }
         },
         scales: {
             x: { title: { display: true, text: 'Tempo (s)' } },
             y: { title: { display: true, text: 'Velocidade (m/s)' } }
-        }
-    }
-});
-
-// gráfico de posição
-const ctxPositionChart = document.getElementById("positionChart").getContext("2d");
-const positionChart = new Chart(ctxPositionChart, {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Posição (m)',
-            data: [],
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 2,
-            fill: false,
-            tension: 0.1,
-            pointRadius: 0 // Remove os pontos nos gráficos
-        }]
-    },
-    options: {
-        responsive: false,
-        maintainAspectRatio: false,
-        plugins: {
-            legend: {
-                display: true, // Mantém a legenda visível
-                labels: {
-                    usePointStyle: false, // Remove o botão de esconder dataset
-                    boxWidth: 0 // Remove a caixinha ao lado do nome
-                }
-            }
         },
-        scales: {
-            x: { title: { display: true, text: 'Tempo (s)' } },
-            y: { title: { display: true, text: 'Posição (m)' } }
+        animation: {
+            easing: 'linear'
         }
     }
 });
@@ -126,8 +101,8 @@ const accelChart = new Chart(ctxAccelChart, {
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 2,
             fill: false,
-            tension: 0.1,
-            pointRadius: 0 // Remove os pontos nos gráficos
+            tension: 0,
+            pointRadius: 0 
         }]
     },
     options: {
@@ -135,229 +110,239 @@ const accelChart = new Chart(ctxAccelChart, {
         maintainAspectRatio: false,
         plugins: {
             legend: {
-                display: true, // Mantém a legenda visível
+                display: true,
                 labels: {
-                    usePointStyle: false, // Remove o botão de esconder dataset
-                    boxWidth: 0 // Remove a caixinha ao lado do nome
+                    usePointStyle: false,
+                    boxWidth: 0
                 }
             }
         },
         scales: {
             x: { title: { display: true, text: 'Tempo (s)' } },
             y: { title: { display: true, text: 'Aceleração (m/s²)' } }
+        },
+        animation: {
+            easing: 'linear'
         }
     }
 });
 
-function modeloCarro(idCarro) {
-    carroImg.src = carros[`carro${idCarro}`].link;
-    carroImg.onload = function() {
-        carroImg.width = carros[`carro${idCarro}`].width;
-        carroImg.height = carros[`carro${idCarro}`].height;
-    };
+// gráfico de posição
+const ctxPositionChart = document.getElementById("positionChart").getContext("2d");
+const positionChart = new Chart(ctxPositionChart, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Posição (m)',
+            data: [],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 2,
+            fill: false,
+            tension: 0,
+            pointRadius: 0
+        }]
+    },
+    options: {
+        responsive: false,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: true,
+                labels: {
+                    usePointStyle: false,
+                    boxWidth: 0 
+                }
+            }
+        },
+        scales: {
+            x: { title: { display: true, text: 'Tempo (s)' } },
+            y: { title: { display: true, text: 'Posição (m)' } }
+        },
+        animation: {
+            easing: 'linear'
+        }
+    }
+});
 
-    document.querySelectorAll('.carIcon').forEach(element => element.classList.remove('active'));
-    document.getElementById(`car${idCarro}`).classList.add('active');
-    carro = `carro${idCarro}`;
-    moverCarro();
+function moverCarro() {
+    s0 = Number(document.getElementById('posInput').value) || 0;
+    document.getElementById('posInput').value = s0;
+
+    track = Number(document.getElementById('trackInput').value) || 100;
+    document.getElementById('trackInput').value = track;
+
+    if(track <= 0 || isNaN(track)) {
+        track = 100;
+        document.getElementById('trackInput').value = 100;
+    }
+
+    if(s0 > track) {
+        s0 = track;
+        document.getElementById('posInput').value = s0;
+    }
+
+    if(s0 < 0) {
+        s0 = 0;
+        document.getElementById('posInput').value = s0;
+    }
+    
+    document.getElementById('halfTrackLength').textContent = `${track/2}m`;
+    document.getElementById('trackLength').textContent = `${track}m`;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // limpa a tela
+    let posX = (s0 / track) * (canvas.width - carroImg.width);
+    ctx.save();
+
+    if(carros[carro].direction === 'left') {
+        ctx.scale(-1, 1);
+        ctx.drawImage(carroImg, -posX - carros[carro].width, carros[carro].posY, carros[carro].width, carros[carro].height);
+    }
+    else {
+        ctx.drawImage(carroImg, posX, carros[carro].posY, carros[carro].width, carros[carro].height); // define a posicao do carro no canvas
+    }
+    ctx.restore();
 }
 
-function atualizarPosicao() {
-    if(v0 < 0 && s0 === 0 || a < 0 && s0 === 0 && v0 <= 0 || v0 === 0 && a === 0) {
-        animating = false;
-        stopped = true;
-        return;
-    }
-    if(stopped && !animating) return;
-    t = Number((t + dt).toFixed(10)); 
-    
-    let s = s0 + v0 * t + 0.5 * a * t ** 2;
+function animarCarro() {
+    t += dt;
     let v = v0 + a * t;
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if(!animando || s0 === 0 && v <= 0 && t === 0) return;
+    framesCount++;
+    let s = s0 + v0 * t + 0.5 * a * t ** 2;
     
-    let posicaoPx = (s / pistaMetros) * (canvas.width - carroImg.width);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // limpa a tela
+
+    let posX = (s / track) * (canvas.width - carroImg.width);
 
     ctx.save();
 
-    if (carros[carro].direction === 'left') {
+    if(s >= track/2 && !midTrack && s0 <= track/2) {
+        if(s0 === track/2) {
+            midTrack = 0;
+        }
+        else midTrack = t;
+        document.getElementById('halfTimer').textContent = `${midTrack.toFixed(3)}s`;
+    }
+    else if(s < track/2) {
+        document.getElementById('halfTimer').textContent = `${t.toFixed(3)}s`;
+    }
+
+    document.getElementById('fullTimer').textContent = `${t.toFixed(3)}s`;
+
+    if(carros[carro].direction === 'left') {
         ctx.scale(-1, 1);
-        ctx.drawImage(carroImg, -posicaoPx - carroImg.width, carros[carro].posY, carroImg.width, carroImg.height);
-    } else {
-        ctx.drawImage(carroImg, posicaoPx, carros[carro].posY, carroImg.width, carroImg.height);
+        ctx.drawImage(carroImg, -posX - carros[carro].width, carros[carro].posY, carros[carro].width, carros[carro].height);
+    }
+    else {
+        ctx.drawImage(carroImg, posX, carros[carro].posY, carros[carro].width, carros[carro].height); // define a posicao do carro no canvas
     }
 
     ctx.restore();
 
-    if (performance.now() - lastGraphUpdate > 50) { 
+    if (framesCount % 5 === 0) { // atualiza os gráficos a cada 5 frames
         speedChart.data.labels.push(t.toFixed(3));
         speedChart.data.datasets[0].data.push(v.toFixed(3));
         speedChart.update();
-
-        positionChart.data.labels.push(t.toFixed(3));
-        positionChart.data.datasets[0].data.push(s.toFixed(3));
-        positionChart.update();
 
         accelChart.data.labels.push(t.toFixed(3));
         accelChart.data.datasets[0].data.push(a.toFixed(3));
         accelChart.update();
 
-        lastGraphUpdate = performance.now();
+        positionChart.data.labels.push(t.toFixed(3));
+        positionChart.data.datasets[0].data.push(s.toFixed(3));
+        positionChart.update();
+
+        document.getElementById('speed').textContent = `${v.toFixed(1)}m/s`;
+        document.getElementById('position').textContent = `${s.toFixed(1)}m`;
     }
 
-    document.getElementById('cronometroTotal').textContent = `${t.toFixed(3)}s`;
-    if (s < (pistaMetros / 2)) {
-        document.getElementById('cronometroMeio').textContent = `${t.toFixed(3)}s`;
-    }
-    if (s >= (pistaMetros / 2)) {
-        if (!halfTime) { 
-            halfTime = t;
-            document.getElementById('cronometroMeio').textContent = `${(halfTime + 0).toFixed(3)}s`;
-        }
-    }
-    
-    if (s >= pistaMetros || (s <= 0 && v < 0)) {
-        animating = false;
-        stopped = true;
+    if(s >= track || s <= 0 && v < 0) {
         t = 0;
-        s0 = parseFloat(document.getElementById('positionInput').value) || 0;
+        midTrack = null;
+        animando = false;
+        document.getElementById('position').textContent = `${track.toFixed(1)}m`;
         return;
     }
 
-    if (animating) {
-        requestAnimationFrame(atualizarPosicao);
-    }
+    requestAnimationFrame(animarCarro);
 }
 
 function start() {
-    if (animating) return;
-    animating = true;
-    stopped = false;
-    halfTime = null;
-    t = 0;
+    if(animando) return;
+    
+    v0 = Number(document.getElementById('speedInput').value) || 0;
+    a = Number(document.getElementById('accelInput').value) || 0;
+    if(!v0 && !a) return;
 
-    s0 = parseFloat(document.getElementById('positionInput').value) || 0;
-    v0 = parseFloat(document.getElementById('speedInput').value) || 0;
-    a = parseFloat(document.getElementById('accelInput').value) || 0;
-
-    // Reseta os gráficos
+    // reseta os gráficos
     speedChart.data.labels = [];
     speedChart.data.datasets[0].data = [];
     speedChart.update();
-
-    positionChart.data.labels = [];
-    positionChart.data.datasets[0].data = [];
-    positionChart.update();
 
     accelChart.data.labels = [];
     accelChart.data.datasets[0].data = [];
     accelChart.update();
 
-    pistaMetros = parseFloat(document.getElementById('courseInput').value) || 100;
+    positionChart.data.labels = [];
+    positionChart.data.datasets[0].data = [];
+    positionChart.update();
 
-    lastGraphUpdate = 0;
-    requestAnimationFrame(atualizarPosicao);
+    animando = true;
+    requestAnimationFrame(animarCarro);
 }
 
-function stop() {
-    if (!animating) { // Se não estiver animando, significa que queremos retomar
-        animating = true;
-        stopped = false;
-        requestAnimationFrame(atualizarPosicao);
-    } else { // Se estiver animando, significa que queremos parar
-        animating = false;
-        stopped = true;
+function pause() {
+    if(animando) {
+        animando = false;
+    }
+    else if(!animando && t != 0) {
+        animando = true;
+        requestAnimationFrame(animarCarro);
     }
 }
 
-function alterarPista() {
-    let halfCourse = document.getElementById('courseInput').value / 2;
-    document.getElementById('halfCourse').textContent = `${halfCourse}m`;
+function reset() {
+    animando = false;
+    midTrack = null;
+    t = 0;
+
+    // reseta os gráficos
+    speedChart.data.labels = [];
+    speedChart.data.datasets[0].data = [];
+    speedChart.update();
+
+    accelChart.data.labels = [];
+    accelChart.data.datasets[0].data = [];
+    accelChart.update();
+
+    positionChart.data.labels = [];
+    positionChart.data.datasets[0].data = [];
+    positionChart.update();
+
     moverCarro();
 }
 
-function moverCarro() {
-    pistaMetros = parseFloat(document.getElementById('courseInput').value) || 100;
-    document.getElementById('courseInput').value = pistaMetros;
-    s0 = Number(document.getElementById('positionInput').value) || 0;
-    document.getElementById('positionInput').value = s0;
-    
-    if (s0 > pistaMetros) {
-        document.getElementById('positionInput').value = pistaMetros;
-        s0 = pistaMetros;
-    }
-
-    if (s0 < 0 || isNaN(s0)) {
-        document.getElementById('positionInput').value = 0;
-        s0 = 0;
-    }
-
-    // Limpar a tela antes de redesenhar
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Determinar a posição em pixels
-    let posicaoPx = (s0 / pistaMetros) * (canvas.width - carroImg.width);
-
-    ctx.save(); // Salva o estado do contexto
-
-    if (carros[carro].direction === 'left') {
-        ctx.scale(-1, 1);
-        ctx.drawImage(carroImg, -posicaoPx - carros[carro].width, carros[carro].posY, carros[carro].width, carros[carro].height);
-    } else {
-        ctx.drawImage(carroImg, posicaoPx, carros[carro].posY, carros[carro].width, carros[carro].height);
-    }
-
-    ctx.restore(); // Restaura o contexto
-}
-
-
-// Chame a função no carregamento da página para garantir que o carro comece na posição inicial
-modeloCarro(1);
-moverCarro();
-
-document.getElementById('positionInput').addEventListener('input', moverCarro);
-
-document.addEventListener("keydown", function(event) {
-    if(event.key === "Enter" && !animating && stopped && t === 0) {
+document.addEventListener("keydown", function(event) { // teclas para ativar os botoes mais facilmente
+    if(event.key === "Enter") { // Enter pra comecar
         document.getElementById('startBttn').classList.add("hover");
         setTimeout(() => document.getElementById('startBttn').classList.remove("hover"), 225);
         setTimeout(() => {start();}, 175);
     }
 
-    else if(event.key === "p") {
+    if(event.key === "p") { // P para pausar
         document.getElementById('stopBttn').classList.add("hover");
         setTimeout(() => document.getElementById('stopBttn').classList.remove("hover"), 175);
-        setTimeout(() => {stop();}, 50);
+        setTimeout(() => {pause();}, 50);
     }
 
-    else if(event.key === "r") {
+    if(event.key === "r") { // R para resetar
         document.getElementById('resetBttn').classList.add("hover");
         setTimeout(() => document.getElementById('resetBttn').classList.remove("hover"), 175);
-        setTimeout(() => {reset();}, 50);
+        setTimeout(() => {reset();}, 75);
     }
     return;
 });
 
-function reset() {
-    animating = false;
-    stopped = true;
-    t = 0;
-    halfTime = null;
-    s0 = 0;
-    v0 = 0;
-    a = 0;
-
-    speedChart.data.labels = [];
-    speedChart.data.datasets[0].data = [];
-    speedChart.update();
-
-    positionChart.data.labels = [];
-    positionChart.data.datasets[0].data = [];
-    positionChart.update();
-
-    accelChart.data.labels = [];
-    accelChart.data.datasets[0].data = [];
-    accelChart.update();
-
-    moverCarro();
-}
+document.getElementById('posInput').value = 0;
+document.getElementById('trackInput').value = 100;
